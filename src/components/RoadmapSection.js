39ab3539d68cc7f2e929d38dfa035d0362584dc9
sonsofcrypto <sonsofcrypto@protonmail.com>
2022-11-15 18:18:00 +0200

    roadmap section

--- src/components/RoadmapSection.js
+++ src/components/RoadmapSection.js
@@ -5,7 +5,7 @@ const RoadmapSection = (props) => {
     const {progress} = props;
     return (
         <div className='roadmap-container'>
-            <div className='roadmap-lines'></div>
+            <div className='roadmap-lines' id='roadmap'></div>
             <div className='roadmap-section content-wrapper content-wrapper-padding section'>
                 <h1>Roadmap</h1>
                 <div className='roadmap-normal'>
@@ -45,11 +45,19 @@ const RoadmapSection = (props) => {
                         />
                         <h2>Phase 2</h2>
                         <ul>
-                            <li>Large directory of popular web3 dapps. Interacting with smart contracts natively in apps. Via decentralized chain networks directly. Not via centralized web2 APIs. Making it possible to interact with some of web3 dapps on mobile for the first time.</li>
-                            <li>Additional web3 browser (interacts via front ends)</li>
-                            <li>Additional L1s support</li>
-                            <li>Additional L2s support</li>
-                            <li>Additional fiat onramps support</li>
+                            <li>Android app</li>
+                            <li>Browser extensions</li>
+                            <li>Large directory of popular web3 dapps. Interacting with smart contracts natively in apps. Via decentralized chain networks directly. Not via centralized <strike>web2</strike> APIs.</li>
+                            <li>Dapps browser</li>
+                            <li>Moar L1s & L2s support</li>
+                            <li>Wallet connect support</li>
+                            <li>Hardware wallets support</li>
+                            <li>Tradfi onramps</li>
+                            <li>
+                                <a href="https://sonsofcrypto.com/web3wallet-improvement-proposals/" target="_blank">
+                                    Click here for details
+                                </a>
+                            </li>
                         </ul>
                     </div>
                     <div className='roadmap-card'>
@@ -62,13 +70,10 @@ const RoadmapSection = (props) => {
                         />
                         <h2>Phase 3</h2>
                         <ul>
-                            <li>Full DAO launch</li>
-                            <li>Moar integrations</li>
-                            <li>Moar L1s & L2s </li>
                             <li>Re-imagining most important web2 services for web3.</li>
                             <li>web3 messenger</li>
                             <li>web3 social media</li>
-                            <li>web3 everything</li>
+                            <li>web3 everything!</li>
                         </ul>
                         <img className='roadmap-card-meme'
                              srcSet={
@@ -78,6 +83,11 @@ const RoadmapSection = (props) => {
                              src={'images/meme-wojak-bliss.png'}
                         />
                     </div>
+                </div>
+                <div className="road-map-wips">
+                    <a href="https://sonsofcrypto.com/web3wallet-improvement-proposals/" target="_blank">
+                        <h3>See all web3wallet improvement proposals</h3>
+                    </a>
                 </div>
             </div>
         </div>
